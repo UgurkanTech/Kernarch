bcf78c31545cf5cce0a741a02dce479be7c4cf93
UgurkanTech <ugurkanstr@gmail.com>
2024-06-16 16:31:35 +0300

    32bits

--- kernel.asm
+++ kernel.asm
@@ -9,7 +9,9 @@ _start:
     mov ds, ax          ; DS = 0
     mov es, ax          ; ES = 0
     mov ss, ax          ; SS = 0
-    mov sp, 0x7c00      ; Set stack pointer to 0x7c00 (top of stack)
+    mov ax, 0x9000  ; Set AX to 0x9000
+    mov ss, ax     ; Set stack segment to 0x9000
+    mov sp, 0xFFFF ; Set stack pointer to 0xFFFF (top of the stack)
     
     mov si, hello
     call print_string
@@ -46,6 +48,8 @@ switch_16_to_32:
     ; Far jump to flush the prefetch queue and switch to 32-bit mode
     jmp CODE_SEG:init32
 
+
+
 [bits 32]
 init32:
     ; Set up data segments
@@ -62,17 +66,33 @@ init32:
     call print_string32
 
     ; Infinite loop to halt the system
-    hlt
+    .loop:
+        jmp .loop
 
 print_string32:
-    mov edx, esi
+    ; Clear the screen
+    mov edi, VIDEO_MEMORY
+    mov ecx, SCREEN_SIZE
+    mov ax, ' ' | (WHITE_ON_BLACK << 8)
+.clear_screen:
+    dec ecx
+    js .done_clear
+    mov [edi], ax
+    add edi, 2
+    jmp .clear_screen
+.done_clear:
+
+    ; Print the string
+    mov edi, VIDEO_MEMORY
+    mov esi, hello32
 .loop32:
-    mov al, [edx]
+    mov al, [esi]
     cmp al, 0
     je .done32
-    mov ah, 0x0E
-    int 0x10
-    inc edx
+    mov ah, GREEN_ON_BLACK
+    mov [edi], ax
+    add esi, 1
+    add edi, 2
     jmp .loop32
 .done32:
     ret
@@ -82,6 +102,10 @@ print_string32:
 
 hello db 'Hello world!', 0x0a, 0
 hello32 db 'Hello from Protected Mode!', 0x0a, 0
+VIDEO_MEMORY equ 0xB8000
+WHITE_ON_BLACK equ 0x0F
+GREEN_ON_BLACK equ 0x02
+SCREEN_SIZE equ 4000 ; 80 columns * 25 rows * 2 bytes per cell
 
 times 510-($-$$) db 0
 dw 0xaa55
