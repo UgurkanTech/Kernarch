3d50a760533869c3fd313d069dc0c6934eb28f9c
UgurkanTech <ugurkanstr@gmail.com>
2024-11-01 21:58:33 +0300

    Update context.asm

--- KernarchOS/kernel/context.asm
+++ KernarchOS/kernel/context.asm
@@ -32,23 +32,46 @@ save_context:
 ; load_context(Context* context)
 ; Does not return to caller, switches to new context
 load_context:
-    mov eax, [esp + 4]       ; Get context pointer from argument
+    mov eax, [esp + 4]            ; Get context pointer from argument
 
-    ; Load segment selectors from context
-    mov dx, [eax + 34]       ; DS
+    ; Load general-purpose registers
+    mov edi, [eax + 0]            ; EDI at Offset 0
+    mov esi, [eax + 4]            ; ESI at Offset 4
+    mov ebp, [eax + 8]            ; EBP at Offset 8
+    mov ebx, [eax + 16]           ; EBX at Offset 16
+    mov edx, [eax + 20]           ; EDX at Offset 20
+    mov ecx, [eax + 24]           ; ECX at Offset 24
+
+    ; Load segment registers
+    mov dx, [eax + 34]            ; DS at Offset 34
     mov ds, dx
+    mov dx, [eax + 36]            ; ES at Offset 36
     mov es, dx
+    mov dx, [eax + 38]            ; FS at Offset 38
     mov fs, dx
+    mov dx, [eax + 40]            ; GS at Offset 40
     mov gs, dx
 
-    ; Prepare stack for iret (sets SS, ESP, EFLAGS, CS, EIP)
-    push dword [eax + 42]    ; SS
-    push dword [eax + 12]    ; ESP
-    push dword [eax + 48]    ; EFLAGS
-    push dword [eax + 32]    ; CS
-    push dword [eax + 44]    ; EIP
+    ; Check if the process is kernel-mode (compare CS to 0x08)
+    cmp word [eax + 32], 0x08     ; CS at Offset 32 for kernel mode
+    je .kernel_mode               ; Jump to kernel mode setup if true
 
-    sti
+.user_mode:
+    ; For user-mode: Push SS and ESP
+    push dword [eax + 42]         ; SS at Offset 42 (only user-mode)
+    push dword [eax + 12]         ; ESP at Offset 12
+    jmp .common_iret              ; Jump to common iret
 
-    ; Execute iret to switch context
-    iretd
+.kernel_mode:
+    ; For kernel mode, only push ESP
+    push dword [eax + 12]         ; ESP at Offset 12 (kernel-mode)
+
+.common_iret:
+    ; Common IRET setup for both modes
+    push dword [eax + 48]         ; EFLAGS at Offset 48
+    push dword [eax + 32]         ; CS at Offset 32
+    push dword [eax + 44]         ; EIP at Offset 44
+    mov eax, [eax + 28]           ; EAX at Offset 28
+
+    sti                            ; Enable interrupts
+    iretd                          ; Execute iret to switch context
