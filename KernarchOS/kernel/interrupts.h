2561b41e93ccc2a8a2198c02552a1177b095fde9
UgurkanTech <ugurkanstr@gmail.com>
2024-11-02 01:37:26 +0300

    Update interrupts.h

--- KernarchOS/kernel/interrupts.h
+++ KernarchOS/kernel/interrupts.h
@@ -10,9 +10,10 @@ enum SoftwareInterrupt {
     INT_OUT_OF_MEMORY = 48,
     INT_STACK_OVERFLOW = 49,
     Yield = 128,
+    Sleep = 129,
 };
 
-void handle_software_interrupt(interrupt_frame* frame);
+void handle_software_interrupt(uint8_t int_no, interrupt_frame* frame) ;
 
 
 
