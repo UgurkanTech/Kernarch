60f2b09f98ca3a7d2f083da98e2b32509c9e5dbd
UgurkanTech <ugurkanstr@gmail.com>
2024-11-13 22:34:52 +0300

    New interrupt handlers

--- KernarchOS/kernel/isr.cpp
+++ KernarchOS/kernel/isr.cpp
@@ -8,175 +8,106 @@
 #include "kernel_config.h"
 #include "io.h"
 #include "string_utils.h"
-#include "logger.h"
 
-const char* exception_messages[] = {
-    "Division By Zero", "Debug", "Non Maskable Interrupt", "Breakpoint",
-    "Into Detected Overflow", "Out of Bounds", "Invalid Opcode", "No Coprocessor",
-    "Double Fault", "Coprocessor Segment Overrun", "Bad TSS", "Segment Not Present",
-    "Stack Fault", "General Protection Fault", "Page Fault", "Unknown Interrupt",
-    "Coprocessor Fault", "Alignment Check", "Machine Check", "SIMD Floating-Point Exception",
-    "Virtualization Exception", "Control Protection Exception"
-};
-
-void print_interrupt_frame(interrupt_frame* frame) {
-    Logger::serial_log("Interrupt Frame:\n"
-        "DS: 0x%x\n"
-        "EDI: 0x%x\n"
-        "ESI: 0x%x\n"
-        "EBP: 0x%x\n"
-        "ESP: 0x%x\n"
-        "EBX: 0x%x\n"
-        "EDX: 0x%x\n"
-        "ECX: 0x%x\n"
-        "EAX: 0x%x\n"
-        "Interrupt Number: %d\n"
-        "Error Code: 0x%x\n"
-        "EIP: 0x%x\n"
-        "CS: 0x%x\n"
-        "EFLAGS: 0x%x\n"
-        "User ESP: 0x%x\n"
-        "SS: 0x%x\n\n",
-        frame->ds, frame->edi, frame->esi, frame->ebp, frame->_,
-        frame->ebx, frame->edx, frame->ecx, frame->eax,
-        0xffff, frame->err_code,
-        frame->eip, frame->cs, frame->eflags,
-        frame->esp, frame->ss
-    );
-}
-
-
-void page_fault_handler(interrupt_frame* frame) {
-    uint32_t faulting_address;
-    asm volatile("mov %%cr2, %0" : "=r" (faulting_address));
-
-    term_print("FATAL: Page Fault!\n");
-    term_print("Faulting address: 0x");
-    term_print_hex(faulting_address);
-    term_print("\nError code: 0x");
-    term_print_hex(frame->err_code);
-    term_print("\nAt EIP: 0x");
-    term_print_hex(frame->eip);
-    term_print(" CS: 0x");
-    term_print_hex(frame->cs);
-    term_print(" SS: 0x");
-    term_print_hex(frame->ss);
-    term_print(" ESP: 0x");
-    term_print_hex(frame->esp);
-
-    term_print("\n");
-
-    if (faulting_address >= (uint32_t)&stack_guard_bottom && faulting_address < (uint32_t)&stack_top) {
-        term_print("Stack overflow detected!\n");
-    } else {
-        term_print("General Page Fault ( ");
-        if (!(frame->err_code & 0x1)) term_print("Page not present ");
-        if (frame->err_code & 0x2) term_print("Write operation");
-        if (frame->err_code & 0x4) term_print("User-mode ");
-        if (frame->err_code & 0x8) term_print("Reserved bits overwritten");
-        if (frame->err_code & 0x10) term_print("Instruction fetch ");
-        term_print(")\n");
-    }
-
-    term_print("System halted.\n");
-
-    // Disable interrupts and halt the CPU
-    //asm volatile("cli; hlt");
-
-    // In case an NMI wakes the CPU, loop forever
-    for(;;);
-}
-
-extern "C" void isr_handler(uint8_t vec, interrupt_frame frame) {
-    uint8_t int_no = vec;
-    if (int_no < 32) {
-        // Handle exceptions
-        switch(int_no) {
-            case EXC_PAGE_FAULT:
-                page_fault_handler(&frame);
-                break;
-            case EXC_DEBUG:
-                term_print("Debug Interrupt Triggered.\n");
-                break;
-            case EXC_DIVIDE_ERROR:
-                term_print("Division by zero exception\n");
-                //frame->eip += 2; // Skip the instruction that caused the exception XD
-                while(1);
-                break;
-
-            case EXC_BOUND_RANGE_EXCEEDED:
-                term_print("Out of bounds exception\n");
-                while(1);
-                break;
-            case EXC_DOUBLE_FAULT:
-                term_print("Double fault occurred! Halting the system.");
-                while(1);
-                break;
-            case EXC_GENERAL_PROTECTION:
-                term_print("General Protection Fault! Halting the system.");
-                print_interrupt_frame(&frame);
-                while (1);
-                break;
-            default:
-                term_print("Unhandled CPU Exception: ");
-                term_print_uint(int_no);
-                term_print(" - ");
-                term_print(exception_messages[int_no]);
-                term_print("\n");
-                print_interrupt_frame(&frame);
-                while (1);
-                break;
-        }
-
-        pic_sendEOI(int_no);
-    } else if (int_no < 48) {
-        // Handle hardware interrupts
-        uint8_t irq = int_no - 32;
-        switch (int_no) {
-            case INT_TIMER:
-                pit_handler(&frame);
-                break;
-            case INT_KEYBOARD:
-                Keyboard::handle_interrupt(&frame);
-                break;
-            case INT_PRIMARY_ATA:
-                break;
-            case INT_SECONDARY_ATA:
-                break;
-            default:
-                term_print("Unhandled hardware interrupt: ");
-                term_print_uint(irq);
-                term_print("\n");
-                break;
-        }
-        pic_sendEOI(irq);
-        
-    } else if (int_no < 256) {
-        // Handle software interrupts
-        handle_software_interrupt(int_no, &frame);
-        
-    } else {
-        term_print("Received unexpected interrupt: ");
-        term_print("Frame address: 0x");
-        term_print_hex((uint32_t)&frame);
-        term_print("\nInt No: 0x");
-        term_print_hex(int_no);
-        term_print(" (");
-        term_print_uint(int_no);
-        term_print(")\nError code: 0x");
-        term_print_hex(frame.err_code);
-        term_print("\nEIP: 0x");
-        term_print_hex(frame.eip);
-        term_print("\n");
-    }
-}
-extern "C" void (*isr_stub_table[])();
+static interrupt_handler_t handlers[256][MAX_HANDLERS_PER_INTERRUPT];
+static uint8_t handler_counts[256];
 
 extern "C" void isr_install() {
     for (int i = 0; i < 256; i++) {
         idt_set_gate(i, (uint32_t)isr_stub_table[i], 0x08, 0x8E);
     }
-
-    // Install the system call handler
     //idt_set_gate(0x80, (uint32_t)isr_stub_table[128], 0x08, 0xEE);  // User mode accessible
+
+    for (int i = 0; i < 256; i++) {
+        handler_counts[i] = 0;
+        for (int j = 0; j < MAX_HANDLERS_PER_INTERRUPT; j++) {
+            handlers[i][j] = NULL;
+        }
+    }
+}
+
+static interrupt_type_t get_interrupt_type(uint8_t vector) {
+    if (vector < HARDWARE_INT_BASE) return INTERRUPT_TYPE_EXCEPTION;
+    if (vector < SOFTWARE_INT_BASE) return INTERRUPT_TYPE_HARDWARE;
+    if (vector < 256) return INTERRUPT_TYPE_SOFTWARE;
+    return INTERRUPT_TYPE_UNKNOWN;
+}
+
+static void handle_exception(uint8_t vector, interrupt_frame* frame) {
+    print_interrupt_frame(frame, vector);
+    term_printf("Kernel Panic! - %s", exception_messages[vector]);
+    while(1) {} // Halt on exception
+}
+
+extern "C" void isr_handler(uint8_t vector, interrupt_frame frame) {
+    interrupt_type_t type = get_interrupt_type(vector);
+    switch (type) {
+        case INTERRUPT_TYPE_EXCEPTION:
+            handle_exception(vector, &frame);
+            break;
+
+        case INTERRUPT_TYPE_HARDWARE: {
+            uint8_t irq = vector - HARDWARE_INT_BASE;
+            // Execute all registered handlers
+            for (int i = 0; i < handler_counts[vector]; i++) {
+                if (handlers[vector][i]) {
+                    handlers[vector][i](&frame);
+                }
+            }
+            pic_sendEOI(irq);
+            break;
+        }
+
+        case INTERRUPT_TYPE_SOFTWARE:
+            // Execute all registered handlers
+            for (int i = 0; i < handler_counts[vector]; i++) {
+                if (handlers[vector][i]) {
+                    handlers[vector][i](&frame);
+                }
+            }
+            break;
+
+        case INTERRUPT_TYPE_UNKNOWN:
+            term_print("Received unexpected interrupt: ");
+            print_interrupt_frame(&frame, vector);
+            break;
+    }
+}
+
+bool register_interrupt_handler(uint8_t vector, interrupt_handler_t handler) {
+    if (!handler || handler_counts[vector] >= MAX_HANDLERS_PER_INTERRUPT) {
+        return false;
+    }
+
+    // Check if handler is already registered
+    for (int i = 0; i < handler_counts[vector]; i++) {
+        if (handlers[vector][i] == handler) {
+            return false; // Handler already registered
+        }
+    }
+
+    handlers[vector][handler_counts[vector]++] = handler;
+    return true;
+}
+
+bool unregister_interrupt_handler(uint8_t vector, interrupt_handler_t handler) {
+    if (!handler || handler_counts[vector] == 0) {
+        return false;
+    }
+
+    // Find and remove the handler
+    bool found = false;
+    for (int i = 0; i < handler_counts[vector]; i++) {
+        if (handlers[vector][i] == handler) {
+            found = true;
+            // Shift remaining handlers down
+            for (int j = i; j < handler_counts[vector] - 1; j++) {
+                handlers[vector][j] = handlers[vector][j + 1];
+            }
+            handler_counts[vector]--;
+            handlers[vector][handler_counts[vector]] = NULL;
+            break;
+        }
+    }
+    return found;
 }
--- KernarchOS/kernel/isr.h
+++ KernarchOS/kernel/isr.h
@@ -5,6 +5,7 @@
 #include "terminal.h"
 #include "idt.h"
 #include "pic.h"
+#include "logger.h"
 
 typedef struct __attribute__((packed)) interrupt_frame{
     uint32_t isr_esp;     // 0: ESP from interrupt_wrapper
@@ -27,18 +28,31 @@ typedef struct __attribute__((packed)) interrupt_frame{
     uint32_t eip;         // 56: Instruction pointer
     uint32_t cs;          // 60: Code segment
     uint32_t eflags;      // 64: CPU flags
-    uint32_t esp;     // 68: User stack pointer (if privilege change)
+    uint32_t esp;         // 68: User stack pointer (if privilege change)
     uint32_t ss;          // 72: Stack segment (if privilege change)
 } interrupt_frame;
-
-
 
 extern "C" void (*isr_stub_table[])();
 
 extern "C" void isr_install();
 extern "C" void isr_handler(uint8_t vec, interrupt_frame frame);
 
-void print_interrupt_frame(interrupt_frame* frame);
+#define MAX_HANDLERS_PER_INTERRUPT 8
+#define EXCEPTION_BASE 0
+#define HARDWARE_INT_BASE 32
+#define SOFTWARE_INT_BASE 48
+
+typedef enum {
+    INTERRUPT_TYPE_EXCEPTION,
+    INTERRUPT_TYPE_HARDWARE,
+    INTERRUPT_TYPE_SOFTWARE,
+    INTERRUPT_TYPE_UNKNOWN
+} interrupt_type_t;
+
+typedef void (*interrupt_handler_t)(interrupt_frame* frame);
+
+bool register_interrupt_handler(uint8_t vector, interrupt_handler_t handler);
+bool unregister_interrupt_handler(uint8_t vector, interrupt_handler_t handler);
 
 // CPU Exceptions
 enum CPUException {
@@ -86,5 +100,65 @@ enum HardwareInterrupt {
     INT_PRIMARY_ATA,
     INT_SECONDARY_ATA
 };
+
+constexpr const char* exception_messages[] = {
+    "Division By Zero", "Debug", "Non Maskable Interrupt", "Breakpoint",
+    "Into Detected Overflow", "Out of Bounds", "Invalid Opcode", "No Coprocessor",
+    "Double Fault", "Coprocessor Segment Overrun", "Bad TSS", "Segment Not Present",
+    "Stack Fault", "General Protection Fault", "Page Fault", "Unknown Interrupt",
+    "Coprocessor Fault", "Alignment Check", "Machine Check", "SIMD Floating-Point Exception",
+    "Virtualization Exception", "Control Protection Exception"
+};
+
+inline static uint32_t read_cr2() { uint32_t cr2; asm volatile("mov %%cr2, %0" : "=r" (cr2)); return cr2; }
+
+inline static void print_interrupt_frame(interrupt_frame* ctx, uint8_t vec = 0xFF) {
+    if (vec != 0xFF) {
+        if (vec < sizeof(exception_messages) / sizeof(char*)) {
+            Logger::serial_log("Exception: %s\n", exception_messages[vec]);
+        } else {
+            Logger::serial_log("Unknown Exception (Interrupt Number: %u)\n", vec);
+        }
+    }
+
+    Logger::serial_log("Interrupt Frame:\n"
+        "EIP:     0x%x\n"
+        "CS:      0x%x\n"
+        "EFLAGS:  0x%x\n"
+        "ESP:     0x%x\n"
+        "SS:      0x%x\n"
+        "EAX:     0x%x\n"
+        "EBX:     0x%x\n"
+        "ECX:     0x%x\n"
+        "EDX:     0x%x\n"
+        "ESI:     0x%x\n"
+        "EDI:     0x%x\n"
+        "EBP:     0x%x\n"
+        "GS:      0x%x\n"
+        "FS:      0x%x\n"
+        "ES:      0x%x\n"
+        "DS:      0x%x\n",
+        ctx->eip, ctx->cs, ctx->eflags, ctx->esp, ctx->ss,
+        ctx->eax, ctx->ebx, ctx->ecx, ctx->edx,
+        ctx->esi, ctx->edi, ctx->ebp,
+        ctx->gs, ctx->fs, ctx->es, ctx->ds
+    );
+
+    if (ctx->err_code != 0xFFFFFFFF) {
+        Logger::serial_log("Error Code: 0x%x\n", ctx->err_code);
+        if (vec == 14) { // Page Fault
+            Logger::serial_log("Page Fault Details: ");
+            if (ctx->err_code & 0x1) Logger::serial_log("Page-Protection Violation ");
+            else Logger::serial_log("Non-present Page ");
+            if (ctx->err_code & 0x2) Logger::serial_log("Write ");
+            else Logger::serial_log("Read ");
+            if (ctx->err_code & 0x4) Logger::serial_log("User Mode ");
+            else Logger::serial_log("Kernel Mode ");
+            if (ctx->err_code & 0x8) Logger::serial_log("Reserved Bit Overwrite ");
+            if (ctx->err_code & 0x10) Logger::serial_log("Instruction Fetch ");
+            Logger::serial_log("\nFaulting Address: 0x%x\n", read_cr2());
+        }
+    }
+}
 
 #endif // ISR_H
