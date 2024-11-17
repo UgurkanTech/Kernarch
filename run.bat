def5a9e32948f4e162a5af63f20e805377511c5f
UgurkanTech <ugurkanstr@gmail.com>
2024-11-17 17:09:13 +0300

    Small changes

--- run.bat
+++ run.bat
@@ -16,10 +16,10 @@ if exist "%disk_image%" (
     echo The file %disk_image% exists.
 ) else (
     echo The file %disk_image% does not exist. Creating it now...
-    qemu-img create -f raw %disk_image% 512M
+    qemu-img create -f raw %disk_image% 128M
     echo File %disk_image% created.
 )
 
-qemu-system-i386 -d pcall,cpu_reset -monitor stdio -serial file:serial_output.log -display default,show-cursor=on -m 2G -netdev user,id=mynet0 -device rtl8139,netdev=mynet0 -cdrom KernarchOS.iso -drive file=disk.img,format=raw,if=ide,index=0
+qemu-system-i386 -no-reboot -d pcall,cpu_reset -monitor stdio -serial file:serial_output.log -display default,show-cursor=on -m 2G -netdev user,id=mynet0 -device rtl8139,netdev=mynet0 -cdrom KernarchOS.iso -drive file=disk.img,format=raw,if=ide,index=0
 
 cd ..
